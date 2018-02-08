72f1a3e2d44363e518f602f58d2099d5380b8128
ericchiu <ericjack7@gmail.com>
2018-02-08 10:11:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/l2018020801/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.student.l2018020801;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        Intent it = new Intent(MainActivity.this,MyService.class);
+        startService(it);
+    }
+}
---
+++ app/src/main/java/com/example/student/l2018020801/MyService.java
@@ -0,0 +1,29 @@
+package com.example.student.l2018020801;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("msg","this is onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("msg","this is onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
